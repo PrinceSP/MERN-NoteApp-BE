118326e9aa4d1e64bf3a5ea00c7529a1dc00e873
PrinceSP <princepasombaran@gmail.com>
2025-07-06 17:12:50 +0700

    feat: setup middleware, DB connection, and endpoint routes

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from "dotenv"
+import { connectDB } from "./config/db.js"
+import notesRouter from './routes/noteRoute.js'
+import userRouter from "./routes/userRoute.js"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5009
+
+app.use(express.json()) //middleware to allow access the request.body contents
+
+app.use("/api/notes", notesRouter)
+app.use("/api/user", userRouter)
+
+app.use("/",(req,res)=>{
+  res.status(200).send("Successfully connecting...")
+})
+
+connectDB().then(()=>{
+  app.listen(PORT, () => {
+    console.log("Listening on port: ", PORT)
+  })
+})
+
