6923e29c0d2573631ef6f2ad6e92091af164a27f
PrinceSP <princepasombaran@gmail.com>
2025-08-07 15:32:43 +0700

    chore: export as default function

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose"
 
-export const connectDB = async () => {
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO)
-    console.log("MongoDB Connected....")
   } catch (error) {
-    console.log(error)
     process.exit(1)
   }
 }
+
+export default connectDB
