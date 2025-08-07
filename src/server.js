68f85f6a7fbb4d20d0366fc62492004a8cf4521c
PrinceSP <princepasombaran@gmail.com>
2025-08-07 17:45:48 +0700

    feat: add cors and rate limiter middlewares,

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import dotenv from "dotenv"
 import cors from "cors"
-import { connectDB } from "./config/db.js"
-import notesRouter from './routes/noteRoute.js'
-import userRouter from "./routes/userRoute.js"
-import rateLimiter from '../middleware/rateLimiter.js'
+import connectDB from "./config/db.js"
+import noteRoute from './routes/noteRoute.js'
+import userRoute from './routes/userRoute.js'
 
 dotenv.config()
 
@@ -12,18 +11,19 @@ const app = express()
 const PORT = process.env.PORT || 5009
 
 //middleware
+app.use(cors({
+  origin:"http://localhost:5173"
+}))
 app.use(express.json()) //middleware to allow access the request.body contents
-app.use(rateLimiter) //middleware to control request rate limit
-app.use(cors())
 
-app.use("/api/notes", notesRouter)
-app.use("/api/user", userRouter)
+app.use("/api/notes", noteRoute )
+app.use("/api/user", userRoute )
 
-app.use("/",(req,res)=>{
+app.use("/", (_, res) => {
   res.status(200).send("Successfully connecting...")
 })
 
-connectDB().then(()=>{
+connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Listening on port: ", PORT)
   })
