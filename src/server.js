06aafa3a90a0eef1a85b523dcb15a6258924a8ac
PrinceSP <princepasombaran@gmail.com>
2025-08-10 21:00:32 +0700

    feat: add condition to detect production mode

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from "dotenv"
 import cors from "cors"
+import path from 'path'
 import connectDB from "./config/db.js"
 import noteRoute from './routes/noteRoute.js'
 import userRoute from './routes/userRoute.js'
@@ -9,15 +10,26 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5009
+const __dirname = path.resolve()
 
 //middleware
-app.use(cors({
-  origin:"http://localhost:5173"
-}))
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: "http://localhost:5173"
+  }))
+}
 app.use(express.json()) //middleware to allow access the request.body contents
 
-app.use("/api/notes", noteRoute )
-app.use("/api/user", userRoute )
+app.use("/api/notes", noteRoute)
+app.use("/api/user", userRoute)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../noteapp-fe/dist")))
+
+  app.get(/(.*)/, (_, res) => {
+    res.sendFile(path.join(__dirname, "../noteapp-fe","dist", "index.html"))
+  })
+}
 
 app.use("/", (_, res) => {
   res.status(200).send("Successfully connecting...")
@@ -28,4 +40,3 @@ connectDB().then(() => {
     console.log("Listening on port: ", PORT)
   })
 })
-
